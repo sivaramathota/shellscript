f205d7dc6daff9a3c5554568d604e19738d84e81
sivaramathota <sivaramathota@gmail.com>
2023-08-20 12:15:10 +0530

    Conditions

--- 06-condtion.sh
+++ 06-condtion.sh
@@ -4,7 +4,10 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]
 then
-   echo "$NUMBER greter then 20"
+
+   echo "$NUMBER is greter then 20"
+
 else
-   echo "$NUMBER greter then 20"
+   
+   echo "$NUMBER is not greter then 20"
 fi
