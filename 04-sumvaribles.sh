42546bc23db46487b965e7472eb07343e626bebb
sivaramathota <sivaramathota@gmail.com>
2023-08-20 11:50:37 +0530

    hello worls script

---
+++ 04-sumvaribles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is the comment, we need to develop a program that can do addition of 2 numbers
+
+PERSON1=$1
+PERSON2=$2
+
+SUM=((PERSON1+PERSON2))
+
+echo "additinol two numbers: $SUM"
