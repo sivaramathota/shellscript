56017591c066a0db8921e8b2c8539363fac5d26b
sivaramathota <sivaramathota@gmail.com>
2023-08-20 12:43:32 +0530

    install software thorugh shellscript

---
+++ 07-install-maven.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR:: please run the script with root access only"
+   exit 1
+if
+
+yum install maven -y
+
+if [ $? -ne 0 ]
+then 
+   echo "install of maven is error"
+   exit 1
+
+else
+  echo "install of maven is success"
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+
+then
+   echo "install of mysql is error"
+   exit 1
+else
+   echo "install of mysql is success"
+fi
+
