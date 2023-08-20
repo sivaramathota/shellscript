93051f0bdd337fe7ec588231992191b44745f835
sivaramathota <sivaramathota@gmail.com>
2023-08-20 11:27:52 +0530

    hello worls script

---
+++ 01-Hallo world.sh
@@ -0,0 +1,10 @@
+echo "This shell script hello world script"
+
+PERSON1=Sivaram
+PERSON2=Kumar
+
+echo "$PERSON2: good moring Brother $PERSON1"
+echo "$PERSON1: good moring Brother $PERSON2"
+echo "$PERSON2: What about you $PERSON1"
+echo "$PERSON2: good you $PERSON1"
+echo "$PERSON1: i am good moring $PERSON2"
