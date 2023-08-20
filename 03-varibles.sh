3f5889291307a91a9188a62148337a69ec4b53bd
sivaramathota <sivaramathota@gmail.com>
2023-08-20 11:45:00 +0530

    hello worls script

---
+++ 03-varibles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "This shell script hello world script"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON2: good moring Brother $PERSON1"
+echo "$PERSON1: good moring Brother $PERSON2"
+echo "$PERSON2: What about you $PERSON1"
+echo "$PERSON2: good you $PERSON1"
+echo "$PERSON1: i am good moring $PERSON2"
